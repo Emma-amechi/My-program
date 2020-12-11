e9475b3f74a9c045ee5964169cbfcf5e88a4943a
Emmanuel Chukwunenye Amaechi <57543919+Emma-amechi@users.noreply.github.com>
2020-12-11 14:44:14 +0100

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,9 @@
+def personal_details():
+    name, age = "Emmanuel Amaechi", 25
+    address = "Ikot Ekpene, Uyo, Nigeria"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+ 
+
+
+
+
